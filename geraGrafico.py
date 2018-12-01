dd1dfa60084f5902442b1a742c358eecc62812c3
Claudio <claudiosilva.cfls@gmail.com>
2018-12-01 15:39:57 -0200

    conserta comparacao

--- geraGrafico.py
+++ geraGrafico.py
@@ -5,11 +5,11 @@ import os
 
 grafico = Grafico.Grafico()
 
-arquivos = os.listdir("Dados/ciclico")
-for arquivo in arquivos:
-    dado = DadosGrafico.DadosGrafico()
-    dado.dadosDeArquivo("Dados/ciclico/"+arquivo)
-    grafico.adicionaDados(dado, "")
+# arquivos = os.listdir("Dados/ciclico")
+# for arquivo in arquivos:
+#     dado = DadosGrafico.DadosGrafico()
+#     dado.dadosDeArquivo("Dados/ciclico/"+arquivo)
+#     grafico.adicionaDados(dado, "")
 
 # arquivos = os.listdir("Dados/linear")
 # for arquivo in arquivos:
@@ -23,17 +23,17 @@ for arquivo in arquivos:
 #     dado.dadosDeArquivo("Dados/convolucional/"+arquivo)
 #     grafico.adicionaDados(dado, "")
 
-# dado = DadosGrafico.DadosGrafico()
-# dado.dadosDeArquivo('Dados/linear/hamming.txt')
-# grafico.adicionaDados(dado, "")
-# dado.dadosDeArquivo('Dados/ciclico/cicl_1,0,0,0,1,0,1.txt')
-# grafico.adicionaDados(dado, "")
-# dado.dadosDeArquivo('Dados/convolucional/conv_4,25,33,37.txt')
-# grafico.adicionaDados(dado, "")
-# dado.dadosDeArquivo('Dados/convolucional/conv_4,25,33,37_pexata.txt')
-# grafico.adicionaDados(dado, "")
-# dado.dadosDeArquivo('Dados/convolucional/conv_4,25,33,37_euclidiano.txt')
-# grafico.adicionaDados(dado, "")
+dado = DadosGrafico.DadosGrafico()
+dado.dadosDeArquivo('Dados/linear/hamming.txt')
+grafico.adicionaDados(dado, "")
+dado.dadosDeArquivo('Dados/ciclico/cicl_1,0,0,0,1,0,1.txt')
+grafico.adicionaDados(dado, "")
+dado.dadosDeArquivo('Dados/convolucional/conv_4,25,33,37.txt')
+grafico.adicionaDados(dado, "")
+dado.dadosDeArquivo('Dados/convolucional/conv_4,25,33,37_pexata.txt')
+grafico.adicionaDados(dado, "")
+dado.dadosDeArquivo('Dados/convolucional/conv_4,25,33,37_euclidiano.txt')
+grafico.adicionaDados(dado, "")
 
 
 
